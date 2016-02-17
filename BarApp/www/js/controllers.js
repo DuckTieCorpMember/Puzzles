cd4ba9d1c4683a1188108b40415dd6ae1bd128cd
DuckTieCorpMember <danyshbek1996@gmail.com>
2016-02-18 01:23:22 +0600

    still working on problem with localStorage

--- BarApp/www/js/controllers.js
+++ BarApp/www/js/controllers.js
@@ -1,4 +1,33 @@
 //window.localStorage['post'] = JSON.stringify(post);
+//var savedContacts = [
+//  {
+//    name: "Tony Stark",
+//    img_src: "img/ironman_III.png",
+//    img_alt: 'iron_man',
+//    desc: "I already have a gf but if you are hot, come and play!"
+//  },
+//  {
+//    name: "Steve Rogers",
+//    img_src: "img/captainamerica.png",
+//    img_alt: 'captain_america',
+//    desc: "I do not believe in that system, but friends insist, so here I am."
+//  },
+//  {
+//    name: "Thor",
+//    img_src: "img/Avengers-Thor-icon.png",
+//    img_alt: 'thor',
+//    desc: "Jane would kill me, but I am just testing this magical screen."
+//  },
+//  {
+//    name: "Hulk",
+//    img_src: "img/hulk.png",
+//    img_alt: 'hulk',
+//    desc: "I will try not to SMASH you."
+//  }
+//];
+//window.localStorage['savedContacts'] = JSON.stringify(savedContacts);
+//var contacts = JSON.parse(window.localStorage['savedContacts'] || '{}');
+
 var savedContacts = [
   {
     name: "Tony Stark",
@@ -25,24 +54,27 @@ var savedContacts = [
     desc: "I will try not to SMASH you."
   }
 ];
+
 window.localStorage['savedContacts'] = JSON.stringify(savedContacts);
-//var contacts = JSON.parse(window.localStorage['savedContacts'] || '{}');
 
-app.controller("myCtrl", ['$scope', function($scope, $route) {
+app.controller("myCtrl", ['$scope', '$window', function($scope, $window) {
 
-  $scope.contacts = JSON.parse(window.localStorage['savedContacts'] || '{}');
+  $scope.contacts = savedContacts;
+  window.localStorage['savedContacts'] = JSON.stringify(savedContacts);
 
   $scope.AddToContacts = function(name, comment, src)
   {
-    console.log(name + " " + comment + " " + src );
+    //console.log(name + " " + comment + " " + src );
     savedContacts.push({
       name: name,
       img_src: src,
       img_alt: name,
       desc: comment
     });
-    console.log($scope.contacts.length);
+    localStorage.clear();
     window.localStorage['savedContacts'] = JSON.stringify(savedContacts);
     $scope.contacts = JSON.parse(window.localStorage['savedContacts'] || '{}');
+    console.log($scope.contacts.length);
+    console.log(savedContacts.length);
   };
 }]);
